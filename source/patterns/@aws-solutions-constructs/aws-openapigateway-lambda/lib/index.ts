57df3ad58b0d38c3bab1ae3244073a03cc0fe63b
Kenny DiFiore <kenny@zbworldwide.com>
2025-04-22 22:15:03 -0400

    fix(aws-openapigateway-lambda): Update lambda permission to use direct API Gateway ARN instead of arnForExecuteApi

--- source/patterns/@aws-solutions-constructs/aws-openapigateway-lambda/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-openapigateway-lambda/lib/index.ts
@@ -145,7 +145,7 @@ export class OpenApiGatewayToLambda extends Construct {
 
       targetInterface.addPermission(`${id}PermitAPIGInvocation`, {
         principal: new iam.ServicePrincipal('apigateway.amazonaws.com'),
-        sourceArn: this.apiGateway.arnForExecuteApi('*')
+        sourceArn: `arn:aws:apigateway:${scope.region}::/restapis/${this.apiGateway.restApiId}`
       });
     });
   }
